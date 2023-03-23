73e089df7cfd1580191e143e2d2a841c90189dc2
Bradley Vaal <67610940+BRADrocket72@users.noreply.github.com>
2023-03-22 23:33:01 -0400

    generalized add ions function

--- BirdCommand/Custom/IonCell.cs
+++ BirdCommand/Custom/IonCell.cs
@@ -26,6 +26,7 @@ namespace BirdCommand.Custom
         {
             ions.Add(ion);
             OnAppearanceChanged(new EventArgs());
+            base.size.Width = ions.Sum(x=> x.Radius) +10;
         }
 
         public List<Ion> GetIons()
@@ -35,59 +36,30 @@ namespace BirdCommand.Custom
 
         internal override void Draw(Graphics g)
         {
-            //base.Draw(g);
-
-
-            if (ions.Count == 1)
-            {
-                var ion = ions[0];
-                //rectangle = new RectangleElement(location.X, location.Y, ion.Radius, ion.Radius);
-                Rectangle r = GetUnsignedRectangle(new Rectangle(location.X, location.Y, ion.Radius, ion.Radius));
+                int xOffset = 0;
+                int largestRadius = ions.Max(x=> x.Radius);
+                foreach(Ion ion in ions)
+                {
+                Rectangle r = GetUnsignedRectangle(new Rectangle(location.X +xOffset, location.Y+(largestRadius-ion.Radius)/2, ion.Radius, ion.Radius));
                 Rectangle rb = new Rectangle(r.X, r.Y, r.Width + 1, r.Height + 1);
                 Brush b = new LinearGradientBrush(rb, Color.FromArgb((int)(255.0f * (opacity / 100.0f)), ion.Color1), Color.FromArgb((int)(255.0f * (opacity / 100.0f)), ion.Color2), LinearGradientMode.Horizontal);
                 g.FillEllipse(b, r);
                 Pen p = new Pen(borderColor, borderWidth);
                 g.DrawEllipse(p, r);
 
-                g.DrawString(this.GetChargeString(), new Font("Trebuchet MS", 12), Brushes.Black, new PointF(Location.X + ion.Radius, Location.Y-5));
-
-                g.DrawString(ion.Symbol, new Font("Trebuchet MS", 12), Brushes.Black, new PointF(Location.X + ion.Radius/2-10, Location.Y + ion.Radius/2-10));
-
-            }
-            else if (ions.Count == 2)
-            {
-                var sodiumIon = ions.Find(io=>io.Name=="Sodium");
-                var chlorineIon = ions.Find(io => io.Name == "Chlorine");
-
-
-                //rectangle = new RectangleElement(location.X, location.Y, sodiumIon.Radius, sodiumIon.Radius);
-                Rectangle r = GetUnsignedRectangle(new Rectangle(location.X, location.Y+(chlorineIon.Radius-sodiumIon.Radius)/2, sodiumIon.Radius, sodiumIon.Radius));
-                Rectangle rb = new Rectangle(r.X, r.Y, r.Width + 1, r.Height + 1);
-                Brush b = new LinearGradientBrush(rb, Color.FromArgb((int)(255.0f * (opacity / 100.0f)), sodiumIon.Color1), Color.FromArgb((int)(255.0f * (opacity / 100.0f)), sodiumIon.Color2), LinearGradientMode.Horizontal);
-                g.FillEllipse(b, r);
-                Pen p = new Pen(borderColor, borderWidth);
-                g.DrawEllipse(p, r);
-
-                g.DrawString(sodiumIon.Symbol, new Font("Trebuchet MS", 12), Brushes.Black, new PointF(Location.X + sodiumIon.Radius / 2 - 10, Location.Y + sodiumIon.Radius / 2 - 10));
-
-
-                //rectangle = new RectangleElement(location.X+sodiumIon.Radius, location.Y - sodiumIon.Radius/2, chlorineIon.Radius, chlorineIon.Radius);
-                r = GetUnsignedRectangle(new Rectangle(location.X + sodiumIon.Radius, location.Y, chlorineIon.Radius, chlorineIon.Radius));
-                rb = new Rectangle(r.X, r.Y, r.Width + 1, r.Height + 1);
-                b = new LinearGradientBrush(rb, Color.FromArgb((int)(255.0f * (opacity / 100.0f)), chlorineIon.Color1), Color.FromArgb((int)(255.0f * (opacity / 100.0f)), chlorineIon.Color2), LinearGradientMode.Horizontal);
-                g.FillEllipse(b, r);
-                p = new Pen(borderColor, borderWidth);
-                g.DrawEllipse(p, r);
-
-                g.DrawString(chlorineIon.Symbol, new Font("Trebuchet MS", 12), Brushes.Black, new PointF(Location.X + sodiumIon.Radius + chlorineIon.Radius / 2 - 10, Location.Y + chlorineIon.Radius / 2 - 10));
-
-                g.DrawString(this.GetChargeString(), new Font("Trebuchet MS", 12), Brushes.Black, new PointF(Location.X + ions.Sum(i=>i.Radius), Location.Y - 5));
-            }
+                g.DrawString(ion.Symbol, new Font("Trebuchet MS", 12), Brushes.Black, new PointF(Location.X + xOffset+  ion.Radius / 2 - 10, Location.Y + ion.Radius / 2 - 10));
+                xOffset= xOffset + ion.Radius;
+                }
+                g.DrawString(this.GetChargeString(), new Font("Trebuchet MS", 12), Brushes.Black, new PointF(Location.X + xOffset, Location.Y-5));
         }
        
         private string GetChargeString()
         {
             int charge = this.GetTotalCharge();
+            if(charge == 0)
+            {
+                return "";
+            }
             if(charge < 0)
             {
                 return Math.Abs(charge).ToString()+"-";
